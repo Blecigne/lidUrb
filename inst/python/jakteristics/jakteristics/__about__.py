8c0cd638182484e9af40d3f2731ecb795c2ac046
Bastien Lecigne <bastienlecigne@pop-os.localdomain>
2022-02-22 13:20:26 -0500

    Remove Jakteristics from the package and ask user to install it independently.

--- inst/python/jakteristics/jakteristics/__about__.py
+++
@@ -1,3 +0,0 @@
-__author__ = "David Caron"
-__email__ = "david.caron@jakarto.com"
-__version__ = "0.5.0"
--- inst/python/jakteristics/jakteristics/__init__.py
+++
@@ -1,5 +0,0 @@
-from .__about__ import __author__, __email__, __version__
-
-from .main import compute_features
-from .constants import FEATURE_NAMES
-from .ckdtree.ckdtree import cKDTree
